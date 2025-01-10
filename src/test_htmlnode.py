28669dabaa9b41b0af9693a72cba65a596677d13
ymbrael <ymbrael@proton.me>
2025-01-09 18:40:13 -0800

    added LeafNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTLMNode(unittest.TestCase):
     def test_eq(self):
@@ -51,6 +51,20 @@ class TestHTLMNode(unittest.TestCase):
             ' href="https://www.google.com" target="_blank"',
         )
 
+    def test_leaf_node_eq(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.tag, "p")
+        self.assertEqual(node.value, "This is a paragraph of text.")
+        self.assertEqual(node.children, None)
+        self.assertEqual(node.props, None)
+    
+    def test_to_html_leaf(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+    
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Text dump here")
+        self.assertEqual(node.to_html(), "Text dump here")
 
 if __name__ == "__main__":
     unittest.main()
