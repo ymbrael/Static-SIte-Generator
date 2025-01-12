7938744e0727ccb5567ed285eeb513b83bccadda
ymbrael <ymbrael@proton.me>
2025-01-11 21:31:11 -0800

    added text_node_to_html_node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -24,6 +24,31 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("These text nodes have different types", TextType.TEXT)
         node2 = TextNode("These text nodes have different types", TextType.CODE)
         self.assertNotEqual(node, node2)
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        t_node = TextNode("This node contains plain text", TextType.TEXT)
+        h_node = text_node_to_html_node(t_node)
+        self.assertEqual(h_node.tag, None)
+        self.assertEqual(h_node.value, "This node contains plain text")
+        self.assertEqual(h_node.props, None)
+
+    def test_link(self):
+        t_node = TextNode("This node contains a link", TextType.LINK, "https://www.boot.dev")
+        h_node = text_node_to_html_node(t_node)
+        self.assertEqual(h_node.tag, "a")
+        self.assertEqual(h_node.value, "This node contains a link")
+        self.assertEqual(h_node.props, {"href": "https://www.boot.dev"})
+
+    def test_image(self):
+        t_node = TextNode("Wow, very test image", TextType.IMAGE, "https://github.com/ymbrael")
+        h_node = text_node_to_html_node(t_node)
+        self.assertEqual(h_node.tag, "img")
+        self.assertEqual(h_node.value, "")
+        self.assertEqual(h_node.props,
+            {"src": "https://github.com/ymbrael", "alt": "Wow, very test image"}
+            )
 
 
 if __name__ == "__main__":
