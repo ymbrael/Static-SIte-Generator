b770b1d2a66969f56f8df64d8c9914cf62c8aa29
ymbrael <ymbrael@proton.me>
2025-05-12 23:11:31 -0700

    added block to block types

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,9 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
-
+from markdown_blocks import (
+    markdown_to_blocks,
+    block_to_block_type,
+    BlockType
+)
 
 class TestMarkdownToHTML(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -50,6 +53,21 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.ULIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.OLIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
 
 if __name__== "__main__":
     unittest.main()
